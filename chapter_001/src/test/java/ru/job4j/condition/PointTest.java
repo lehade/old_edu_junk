6222d0b66f736c1cb4bf2af9c70f759b82cb4058
Alexey Denisenko <alexey.denisenko@me.com>
2018-12-22 00:13:08 +0300

    Дописал тест на класс расчета расстояния между точками

---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,20 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+    @Test
+    public void pointTest() {
+
+        Point a = new Point(10, 5);
+        Point b = new Point(-3, -3);
+        double result = a.distanceTo(b);
+        double expected = 15.264337522473747;
+        assertThat(result, is(expected));
+
+    }
+
+}
