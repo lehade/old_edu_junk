9d759f99cc0357be89b04fb581346b57a34b695e
Alexey Denisenko <alexey.denisenko@me.com>
2018-12-23 17:21:24 +0300

    Поправил опечатку в названии тестового метода, Дописал класс бота.

---
+++ chapter_001/src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,24 @@
+package ru.job4j.condition;
+
+/**
+ * @author Alexey Denisenko
+ * @version $Id$
+ * @since 21.12.2018
+ */
+public class DummyBot {
+    /**
+     * Отвечает на вопросы.
+     *
+     * @param question Вопрос от клиента.
+     * @return Ответ.
+     */
+    public String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Спросите другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
