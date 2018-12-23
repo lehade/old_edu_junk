828a9cbae34ce5d44246f742f7931422e1c16c0b
Alexey Denisenko <alexey.denisenko@me.com>
2018-12-23 20:15:39 +0300

    упражнение на тернарный оператор

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,4 +1,18 @@
 package ru.job4j.max;
 
+/**
+ * @author Alexey Denisenko
+ * @version 0.1
+ * @since 23.12.2018
+ */
+
 public class Max {
+
+    public int max(int first, int second) {
+
+        int max = first > second ? first : second;
+
+        return max;
+    }
+
 }
--- chapter_001/src/test/java/ru/job4j/max/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -1,4 +1,23 @@
 package ru.job4j.max;
 
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexey Denisenko
+ * @version 0.1
+ * @since 23.12.2018
+ */
+
 public class MaxTest {
+
+    @Test
+    public void whenFirstLessSecond() {
+        Max maxim = new Max();
+        int result = maxim.max(5, 9);
+        assertThat(result, is(9));
+    }
+
 }
