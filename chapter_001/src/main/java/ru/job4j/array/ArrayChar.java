14cd22ff99bd3bdc5ff18479ebe18fb8e7b2f08b
Alexey Denisenko <alexey.denisenko@me.com>
2018-12-29 00:40:02 +0300

    Исправления по замечаниям

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -15,18 +15,17 @@ public class ArrayChar {
     }
 
     /**
-     * Проверяет. что слово начинается с префикса.
+     * Проверяем, что массив data имеет первые элементы одинаковые с value
      * @param prefix префикс.
      * @return если слово начинается с префикса
      */
     public boolean startWith(String prefix) {
         boolean result = true;
         char[] value = prefix.toCharArray();
-        // проверить. что массив data имеет первые элементы одинаковые с value
-        //this.data = prefix.toCharArray();
         for (int i = 0; i < value.length; i++) {
             if (data[i] != value[i]) {
                 result = false;
+                break;
             }
         }
         return result;
