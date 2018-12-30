2d7cdc7c9e70851ad2a9b87843366f1ae06a8a87
Alexey Denisenko <alexey.denisenko@me.com>
2018-12-30 22:42:45 +0300

    удалил остатки экспериметов

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -13,7 +13,6 @@ public class ArrayDuplicate {
     public String[] removeDuplicates(String[] array) {
         int duplicate = array.length;
         for (int i = 0; i < duplicate; i++) {
-            //String element = array[i];
             for (int j = i + 1; j < duplicate; j++) {
                 if (array[i].equals(array[j])) {
                     array[j] =  array[duplicate - 1];
