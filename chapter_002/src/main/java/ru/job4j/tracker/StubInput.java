a40dc3d7137100bff2f27d4fd544bfe109d4eaee
Alexey Denisenko <alexey.denisenko@me.com>
2019-01-08 17:13:56 +0300

    Реализация тестов для класса консольного ввода

---
+++ chapter_002/src/main/java/ru/job4j/tracker/StubInput.java
@@ -0,0 +1,21 @@
+package ru.job4j.tracker;
+
+/**
+ * @author Alexey Denisenko
+ * @version 0.1
+ * @since 08.01.2019
+ */
+
+public class StubInput implements Input{
+
+    private String[] answers;
+    private int position = 0;
+
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
+
+    public String ask(String question) {
+        return answers[position++];
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,66 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Alexey Denisenko
+ * @version 0.1
+ * @since 08.01.2019
+ */
+
+public class StartUITest {
+
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[0].getName(), is("test name"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+
+        //Тут валится тест, из- за обнуления id при редактировании заявки
+
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+
+    @Test
+    public void getById() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Input input = new StubInput(new String[]{"4", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item.getId()).getName(), is("test name"));
+    }
+
+    @Test
+    public void getByName() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item1 = tracker.add(new Item("test name1", "desc"));
+        Input input = new StubInput(new String[]{"5", item.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findAll()[1].getName(), is("test name1"));
+    }
+
+    @Test
+    public void deleteItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test name", "desc"));
+        Item item2 = tracker.add(new Item("test name2", "desc2"));
+        Item item3 = tracker.add(new Item("test name3", "desc3"));
+        Input input = new StubInput(new String[]{"3", item2.getId(), "6"});
+        new StartUI(input, tracker).init();
+        tracker.delete(item2.getId());
+        assertThat(tracker.findAll()[1].getName(), is("test name3"));
+    }
+}
