025ffde1a270651d57fcdd1ab8c3b8e2040409e8
Alexey Denisenko <alexey.denisenko@me.com>
2019-01-30 23:35:00 +0300

    перенес тесты в отдельный каталог

--- chapter_002/src/test/java/ru/job4j/tracker/singleton/SingleEnumTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/singleton/SingleEnumTest.java
@@ -1,6 +1,7 @@
-package ru.job4j.tracker;
+package ru.job4j.tracker.singleton;
 
 import org.junit.Test;
+import ru.job4j.tracker.Item;
 import ru.job4j.tracker.singleton.TrackerSingleEnum;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
